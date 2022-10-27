6a08b898376e52e030e87ad8ff3552d9db1c1925
dbwjd <dbwjd20111@naver.com>
2022-10-27 14:02:58 +0900

    회원가입

---
+++ login/signup.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#business").click(function(){
+        location.href = "/business_login";
+    });
+    $("#consumer").click(function(){
+        location.href = "/consumer_login";
+    });
+});
+
